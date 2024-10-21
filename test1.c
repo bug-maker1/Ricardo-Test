140b82a5baaf1b21dc4f01a6993ce12dfc23fe23
bug-maker1 <18227887283@163.com>
2024-10-21 17:43:50 +0800

    添加了Plus2Num来计算两数之和

--- test1.c
+++ test1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+int Plus2Num(int num1,int num2)
+{
+    printf("%d",num1 + num2);
+    return 0;
+} 
+
 int main()
 {
-    printf("hello,world");
+    printf("hello,world\n");
+    Plus2Num(1,2);
     return 0;
 }
-
